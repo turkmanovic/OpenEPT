5fbb19642519f7d83fba45552f1d9df33d6c05ca
Haris Turkmanovic <haris.turkmanovic@gmail.com>
2023-11-04 00:18:38 +0100

    Debug folder deleted

--- Firmware/ADFirmware/CM7/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- C:/Users/Haris/Documents/OpenEPT/OpenEPT/Firmware/ADFirmware/Drivers/BSP/Components/lan8742/lan8742.c \
- C:/Users/Haris/Documents/OpenEPT/OpenEPT/Firmware/ADFirmware/Drivers/BSP/Components/lan8742/lan8742.h
-C:/Users/Haris/Documents/OpenEPT/OpenEPT/Firmware/ADFirmware/Drivers/BSP/Components/lan8742/lan8742.h:
