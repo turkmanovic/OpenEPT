bb63fe0a28bdb375e63c2430ebf2ed29e0152c45
Haris Turkmanovic <haris.turkmanovic@gmail.com>
2023-11-04 00:17:53 +0100

    Initial firmware structure added

---
+++ Firmware/ADFirmware/CM7/Debug/Drivers/BSP/Components/lan8742.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Components/lan8742.o: \
+ C:/Users/Haris/Documents/OpenEPT/OpenEPT/Firmware/ADFirmware/Drivers/BSP/Components/lan8742/lan8742.c \
+ C:/Users/Haris/Documents/OpenEPT/OpenEPT/Firmware/ADFirmware/Drivers/BSP/Components/lan8742/lan8742.h
+C:/Users/Haris/Documents/OpenEPT/OpenEPT/Firmware/ADFirmware/Drivers/BSP/Components/lan8742/lan8742.h:
