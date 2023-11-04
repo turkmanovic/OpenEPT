7b356475428a15a8e0841a4cea30bf8c5d7e84bb
Haris Turkmanovic <haris.turkmanovic@gmail.com>
2023-11-04 23:14:37 +0100

    Ethernet Updated

--- Firmware/ADFirmware/CM7/Core/Src/main.c
+++ Firmware/ADFirmware/CM7/Core/Src/main.c
@@ -215,16 +215,11 @@ void SystemClock_Config(void)
 
   /** Supply configuration update enable
   */
-  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY);
 
   /** Configure the main internal regulator output voltage
   */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-
-  __HAL_RCC_SYSCFG_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
   while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
 
--- Firmware/ADFirmware/CM7/Core/Src/stm32h7xx_it.c
+++ Firmware/ADFirmware/CM7/Core/Src/stm32h7xx_it.c
@@ -154,7 +154,6 @@ void DebugMon_Handler(void)
 
   /* USER CODE END DebugMonitor_IRQn 1 */
 }
-
 /******************************************************************************/
 /* STM32H7xx Peripheral Interrupt Handlers                                    */
 /* Add here the Interrupt Handlers for the used peripherals.                  */
