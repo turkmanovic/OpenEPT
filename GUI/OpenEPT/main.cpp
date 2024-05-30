f1977111d9f1db1a961dd441d01925f121200ad4
haris <haris.turkmanovic@gmail.com>
2024-05-30 10:10:57 +0200

    Remove part of the code

--- GUI/OpenEPT/main.cpp
+++ GUI/OpenEPT/main.cpp
@@ -5,12 +5,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-    // Set up OpenGL format
-    QSurfaceFormat format;
-    format.setSamples(4); // Enable 4x multisampling for anti-aliasing
-    QSurfaceFormat::setDefaultFormat(format);
     OpenEPT w;
     w.show();
     return a.exec();
